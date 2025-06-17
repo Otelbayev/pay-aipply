9aab8e41deedc84e7e89fffcdb12b497125d22a5
jasurbek <jasurdev1604@gmail.com>
2025-06-17 12:11:11 +0500

    fix bug

--- js/thankyou.js
+++ js/thankyou.js
@@ -1,4 +1,19 @@
+const prices = {
+  standart: "590,000",
+  premium: "790,000",
+  viponline: "1,390,000",
+  vipoffline: "1,390,000",
+};
+
+const types = {
+  standart: "Standart",
+  premium: "Premium",
+  viponline: "VIP online",
+  vipoffline: "VIP offline",
+};
+
 const params = new URLSearchParams(window.location.search);
 document.getElementById("userName").textContent = params.get("name") || "None";
 document.getElementById("userPhone").textContent = params.get("phone") || "-";
-document.getElementById("userType").textContent = params.get("type") || "-";
+document.getElementById("userType").textContent =
+  types[params.get("type")] || "-";
